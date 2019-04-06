a69667ca748302c016ec97017ea33195d8390058
unknown <acarolcolombo@gmail.com>
2019-04-05 21:49:08 -1000

    exerc 6

--- exercises.js
+++ exercises.js
@@ -127,7 +127,12 @@ console.log(quotient);
  * Console.log your result.
 */
 
+function checkSum(x){
+    return "Mariah Carey has been married " + x + " times."
+}
 
+var daDiva = checkSum(bango1);
+console.log(daDiva);
 
 /*
  * #7
